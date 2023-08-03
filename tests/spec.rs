6ec36a6c883b051a4bedd58e2efa29075c7a3a04
Michael Williams <michael.williams@enspiral.com>
2023-08-04 01:56:48 +1200

    organize multiple specs per file

--- tests/spec.rs
+++ tests/spec.rs
@@ -1,36 +1,42 @@
+use pretty_assertions::assert_eq;
 use rimu::{from_value, Context, Engine, Template, Value};
 use std::{fs, path::Path};
 
 fn test_spec(path: &Path) -> datatest_stable::Result<()> {
     let content = fs::read_to_string(path)?;
-    let value: Value = serde_yaml::from_str(&content)?;
+    let specs: Value = serde_yaml::from_str(&content)?;
 
-    let Value::Object(value) = value else {
-        panic!("Spec should be object");
+    let Value::List(specs) = specs else {
+        panic!("Specs should be list");
     };
 
-    let template: Value = value
-        .get("template")
-        .expect("template should exist")
-        .clone();
-    let context_val: Value = value.get("context").expect("context should exist").clone();
-    let output: Value = value.get("output").expect("output should exist").clone();
+    for spec in specs {
+        let Value::Object(spec) = spec else {
+            panic!("Spec should be object");
+        };
 
-    let template: Template = from_value(template)?;
+        let title: Value = spec.get("title").expect("title should exist").clone();
+        let template: Value = spec.get("template").expect("template should exist").clone();
+        let context_val: Value = spec.get("context").expect("context should exist").clone();
+        let output: Value = spec.get("output").expect("output should exist").clone();
 
-    let engine = Engine::default();
+        let title: String = from_value(title)?;
+        let template: Template = from_value(template)?;
 
-    let mut context = Context::new();
-    let Value::Object(context_obj) = context_val else {
-        panic!("context.yml must be object");
-    };
-    for (key, value) in context_obj.into_iter() {
-        context.insert(key, value);
+        let engine = Engine::default();
+
+        let mut context = Context::new();
+        let Value::Object(context_obj) = context_val else {
+            panic!("context.yml must be object");
+        };
+        for (key, value) in context_obj.into_iter() {
+            context.insert(key, value);
+        }
+
+        let actual: Value = engine.render(&template, &context)?;
+
+        assert_eq!(output, actual, "{}", title);
     }
-
-    let actual: Value = engine.render(&template, &context)?;
-
-    assert_eq!(output, actual);
 
     Ok(())
 }
