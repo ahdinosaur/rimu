dc72268e5eff98b34a95d5159855805ab227f70e
Michael Williams <michael.williams@enspiral.com>
2023-08-05 17:45:16 +1200

    handle errors in spec data tests

--- tests/spec.rs
+++ tests/spec.rs
@@ -15,10 +15,12 @@ fn test_spec(path: &Path) -> datatest_stable::Result<()> {
             panic!("Spec should be object");
         };
 
-        let title: Value = spec.get("title").expect("title should exist").clone();
-        let template: Value = spec.get("template").expect("template should exist").clone();
-        let context_val: Value = spec.get("context").expect("context should exist").clone();
-        let output: Value = spec.get("output").expect("output should exist").clone();
+        let title: Value = spec.get("title").expect("Spec missing 'title'").clone();
+        let template: Value = spec
+            .get("template")
+            .expect("Spec missing 'template'")
+            .clone();
+        let context_val: Value = spec.get("context").expect("Spec missing 'context'").clone();
 
         let title: String = from_value(title)?;
         let template: Template = from_value(template)?;
@@ -27,15 +29,34 @@ fn test_spec(path: &Path) -> datatest_stable::Result<()> {
 
         let mut context = Context::new();
         let Value::Object(context_obj) = context_val else {
-            panic!("context.yml must be object");
+            panic!("Spec 'context' must be object");
         };
         for (key, value) in context_obj.into_iter() {
             context.insert(key, value);
         }
 
-        let actual: Value = engine.render(&template, &context)?;
+        if let Some(output) = spec.get("output") {
+            let actual = engine
+                .render(&template, &context)
+                .expect("Expected render output");
 
-        assert_eq!(output, actual, "{}", title);
+            assert_eq!(output.clone(), actual, "{} : output", title);
+        } else if let Some(error) = spec.get("error") {
+            let Value::Object(error) = error else {
+                panic!("Spec 'error' should be object");
+            };
+            let Value::String(message) = error.get("message").expect("Spec missing 'error.message'") else {
+                panic!("Spec 'error.message' should be string");
+            };
+
+            let actual = engine
+                .render(&template, &context)
+                .expect_err("Expected render error");
+
+            assert_eq!(message, &actual.to_string(), "{} : error name", title);
+        } else {
+            panic!("Spec missing 'output' or 'error'")
+        }
     }
 
     Ok(())
