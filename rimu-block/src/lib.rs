6bc80ab2486969db673daa69e0e13c1eefc0c8d7
Michael Williams <michael.williams@enspiral.com>
2023-09-01 22:36:25 +1200

    remove old notes

--- rimu-block/src/lib.rs
+++ rimu-block/src/lib.rs
@@ -1,27 +1,3 @@
-// maybe this shouldn't use chumsky.
-//
-// state
-// - current indentation
-//
-// parse each line
-//   - get indentation (relative to current)
-//   - get type
-//     - just use regexes
-//     - if has ":" (not inside string), then object entry
-//     - if starts with "-", then list item
-//
-// tokens:
-// - indent
-// - dedent
-// - key
-// - value
-// - list item
-//
-// doc:
-// - object
-// - list
-// - expression: string
-
 use compiler::{compile, CompilerError, SpannedBlock};
 use lexer::{tokenize, LexerError};
 use rimu_report::{SourceId, Span};
