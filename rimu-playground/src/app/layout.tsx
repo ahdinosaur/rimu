bc3dc70f9fbb96bbf4facbd79ec905531d899556
Michael Williams <michael.williams@enspiral.com>
2023-09-13 00:09:11 +1200

    just use default fonts, save a load

--- rimu-playground/src/app/layout.tsx
+++ rimu-playground/src/app/layout.tsx
@@ -1,10 +1,7 @@
 import './globals.css'
 
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import { App } from '@/components/app'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Rimu Playground',
@@ -14,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <App>{children}</App>
       </body>
     </html>
