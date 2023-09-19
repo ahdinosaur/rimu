83ee773f6b144c1e07788ba1d9ba819e02f23538
Michael Williams <michael.williams@enspiral.com>
2023-09-20 00:58:28 +1200

    highlights

---
+++ syntax/lezer/src/highlight.js
@@ -0,0 +1,35 @@
+// with help from
+//
+// - https://github.com/lezer-parser/javascript/blob/main/src/highlight.js
+
+import { styleTags, tags as t } from '@lezer/highlight'
+
+export const jsHighlight = styleTags({
+  'if then else': t.controlKeyword,
+  'let in': t.definitionKeyword,
+
+  Null: t.null,
+  Boolean: t.bool,
+  String: t.string,
+  Number: t.number,
+
+  ArithmeticOp: t.arithmeticOperator,
+  LogicOp: t.logicOperator,
+  // BitOp: t.bitwiseOperator,
+  CompareOp: t.compareOperator,
+  FatArrow: t.function(t.punctuation),
+
+  Identifier: t.variableName,
+  LetKey: t.definition(t.variableName),
+  GetIdentifier: t.propertyName,
+  ObjectKey: t.definition(t.propertyName),
+  FunctionParam: t.definition(t.variableName),
+
+  LineComment: t.lineComment,
+
+  '( )': t.paren,
+  '[ ]': t.squareBracket,
+  '{ }': t.brace,
+  ',': t.separator,
+  '.': t.derefOperator,
+})
