1c73ee027a55e59489a7efc7b81be9d1c0565b6e
Michael Williams <hello@mikey.nz>
2025-10-22 14:43:22 +1300

    Expose rimu::call

--- rimu/src/lib.rs
+++ rimu/src/lib.rs
@@ -1,7 +1,9 @@
 pub use rimu_ast::{
     BinaryOperator, Block, Expression, SpannedBlock, SpannedExpression, UnaryOperator,
 };
-pub use rimu_eval::{evaluate_block as evaluate, evaluate_block, evaluate_expression, EvalError};
+pub use rimu_eval::{
+    call, evaluate_block as evaluate, evaluate_block, evaluate_expression, EvalError,
+};
 pub use rimu_meta::{ErrorReport, ErrorReports, SourceId, Span, Spanned};
 pub use rimu_parse::{parse_block, parse_block as parse, parse_expression, Error as ParseError};
 pub use rimu_stdlib::create_stdlib;
